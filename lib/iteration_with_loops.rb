6f28aef5341274acc64b7b69e6b59089f0a08759
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-07 18:20:10 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,15 +4,14 @@ def join_nested_strings(src)
   while row < src.length do
     count = 0 
     while count < src[row].length do
-      if src[row][count].to_i == 0 
-        result = result.concat(src[row][count])
-        result = result.concat(" ")
+      if src[row][count].class == String
+        result += src[row][count] + " "
       end
       count += 1 
     end
   row += 1 
   end
-  result
+  result.rstrip
 end
 
 test = [[1, 3, 5], [6, 0, 1], ['hello', 3, 'there']]
